c0f967158d7c058490b146379faafaf229caf172
Pedro-Sena-P <sena.pedro@yahoo.com.br>
2025-06-09 20:45:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function msg(){
-    alert("Sr(a) " + document.fale_conosco.nome.value + ", seus dados foram enviados com sucesso.")
+    alert("Sr(a) " + document.fale_conosco.inputNome.value + ", seus dados foram enviados com sucesso.")
+}
+
+function tel(objeto){
+    if(objeto.value.length == 0){
+        objeto.value = objeto.value + '(';
+    }
+    if(objeto.value.length == 3){
+        objeto.value = objeto.value + ')';
+    }
+    if(objeto.value.length == 9){
+        objeto.value = objeto.value + '-';
+    }
 }
